e1b577fc8cd469dc54201cf1003fd98ae09c08f6
reactionic127 <richard.gill127@gmail.com>
2022-11-09 10:51:46 +0800

    added unit testing configuration

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -1,14 +1,7 @@
-/**
- * @format
- */
-
-import 'react-native';
-import React from 'react';
+import * as React from 'react';
 import App from '../src/App';
+import {render} from '@testing-library/react-native';
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
-
-it('renders correctly', () => {
-  renderer.create(<App />);
+test('renders App correctly', async () => {
+  render(<App />);
 });
---
+++ jest-setup.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-undef */
+import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
+
+// Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
+jest.mock('redux-persist', () => {
+  const real = jest.requireActual('redux-persist');
+  return {
+    ...real,
+    persistReducer: jest
+      .fn()
+      .mockImplementation((config, reducers) => reducers),
+  };
+});
